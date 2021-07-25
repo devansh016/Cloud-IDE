5c7c31b8ab07f21e3457ea5e18390bda5f9c3d76
Devansh Chaudhary <devanshchaudhary2002@gmail.com>
2021-07-25 13:39:28 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+var cors = require('cors')
 const port = process.env.PORT || 80;
 
 const app = express();
-app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 
 app.listen(port);
 console.log("Server listening on port "+ port);
