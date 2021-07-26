2f67788e5baa549a84a745b49ce592f9223fe488
Devansh Chaudhary <devanshchaudhary2002@gmail.com>
2021-07-26 22:41:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 var cors = require('cors')
+const path = require('path');
 const port = process.env.PORT || 80;
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
+
+//Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port);
 console.log("Server listening on port "+ port);
