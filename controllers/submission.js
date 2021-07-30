f4d21304acd352445c13678e47f85a58d465fea0
Devansh Chaudhary <devanshchaudhary2002@gmail.com>
2021-07-30 16:48:32 +0530

    update compiler api

--- controllers/submission.js
+++ controllers/submission.js
@@ -12,55 +12,58 @@ function sleep(ms) {
 // Gets a token from the compiler server
 async function executecode(userData){
     var da;
-    let data_send = {
-        source_code: userData.source_code,
-        language_id: userData.language_id,
-        number_of_runs: "1",
-        stdin: userData.stdin,
-        expected_output: null,
-        cpu_time_limit: "2",
-        cpu_extra_time: "0.5",
-        wall_time_limit: "5",
-        memory_limit: "128000",
-        stack_limit: "64000",
-        max_processes_and_or_threads: "60",
-        enable_per_process_and_thread_time_limit: false,
-        enable_per_process_and_thread_memory_limit: false,
-        max_file_size: "1024",
+      var options = {
+        method: 'POST',
+        url: 'https://judge0-ce.p.rapidapi.com/submissions',
+        params: {base64_encoded: 'false', fields: '*'},
+        headers: {
+          'content-type': 'application/json',
+          'x-rapidapi-key': process.env.rapidapikey,
+          'x-rapidapi-host':  process.env.rapidapihost
+        },
+        data: {
+          source_code: userData.source_code,
+          language_id: userData.language_id,
+          number_of_runs: "1",
+          stdin: userData.stdin,
+          expected_output: null,
+          cpu_time_limit: "2",
+          cpu_extra_time: "0.5",
+          wall_time_limit: "1",
+          memory_limit: "128000",
+          stack_limit: "64000",
+          max_processes_and_or_threads: "60",
+          enable_per_process_and_thread_time_limit: false,
+          enable_per_process_and_thread_memory_limit: false,
+          max_file_size: "1024",
+        }
       };
-
-      var config = {
-        method: 'post',
-        url:  compiler_url + '/submissions',
-        headers: { },
-        data: data_send
-      };
-
-      await axios(config)
-      .then(function (response) {
-        da = response.data;   
-      })
-      .catch(function (error) {
-        return (error);
+      await axios.request(options).then(function (response) {
+        da = response.data;
+      }).catch(function (error) {
+        console.error(error);
       });
-      await sleep(5000);
-      return getoutput(da);
+      await sleep(1000);
+      return getoutput(da.token);
 }
 
 // Function to get the details of the executed program using the token
 async function getoutput(da){
   var ans;
-  var config = {
-    method: 'get',
-    url: compiler_url + '/submissions/' + da.token + '?base64_encoded=true',
-    headers: { }
+  var options = {
+    method: 'GET',
+    url: 'https://judge0-ce.p.rapidapi.com/submissions/' + da,
+    params: {base64_encoded: 'true', fields: '*'},
+    headers: {
+      'x-rapidapi-key': process.env.rapidapikey,
+      'x-rapidapi-host':  process.env.rapidapihost
+    }
   };
-  await axios(config)
-  .then(function (response) {
+  
+  await axios.request(options).then(function (response) {
     ans = response.data;
-  })
-  .catch(function (error) {
-    ans = error;
+  }).catch(function (error) {
+    console.error(error);
   });
 
   //Decoding base64 strings
